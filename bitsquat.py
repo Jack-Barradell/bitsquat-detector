b1de9662fea51534f40224f74788fb065f18d8ba
Jack Barradell <jirbarradell-johns1@sheffield.ac.uk>
2018-07-17 01:13:03 +0100

    Initial commit

---
+++ bitsquat.py
@@ -0,0 +1,17 @@
+import re
+
+def usage():
+    print("Bitsquat Detector")
+    print()
+    print("Usage: bitsquat.py <site name>")
+    print("Only the name of the url is needed. E.g for www.google.com, only provide \"google\"")
+    print("E.g: bitsquat.py google")
+    print()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
